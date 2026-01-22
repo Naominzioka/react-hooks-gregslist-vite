3dea5720b36b0c2b78f49dd6307486572dc975c1
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-01-22 19:53:41 +0300

    display all listings

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,18 +1,20 @@
-function ListingCard() {
+
+
+function ListingCard({description, image, location, favourite}) {
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
+        {favourite ? (
           <button className="emoji-button favorite active">★</button>
         ) : (
           <button className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{description}</strong>
+        <span> · {location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
